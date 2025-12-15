693eac7d5bcb7cf2c559e36c2c9b9d1d0b1eb9ac
Dmpecci <davidmpe2002@gmail.com>
2025-12-15 19:55:37 +0100

    Esqueleto lectura/escritura json

--- staticconfiguration/config_base/static_config_base.py
+++ staticconfiguration/config_base/static_config_base.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from .static_config_interface import StaticConfigInterface
 
 class StaticConfigBase(StaticConfigInterface):
-    """Abstract base class for static configuration implementations."""
-
-    pass
+    def get(data_name: str):
+        raise NotImplementedError("Subclasses must implement the get method.")
+    
+    def set(data_name: str, new_value: any):
+        raise NotImplementedError("Subclasses must implement the set method.")
---
+++ staticconfiguration/json_backend/json_backend.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from pathlib import Path
+from staticconfiguration.entities import Data
+
+class JSONBackend:
+    def ensure_initialized(self, config_path: str, config_file: str, version: str, data_fields: list[Data]) -> None:
+        raise NotImplementedError("Subclasses must implement the get method.")
+
+    def read_value(self, data: Data, config_path: Path):
+        raise NotImplementedError("Subclasses must implement the get method.")
+
+    def write_value(self, data: Data, new_value, config_path: Path):
+        raise NotImplementedError("Subclasses must implement the get method.")
