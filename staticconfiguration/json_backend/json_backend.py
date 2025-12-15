08e7d1356fef9aa8175882a4da93c58e9868932e
Dmpecci <davidmpe2002@gmail.com>
2025-12-15 20:06:10 +0100

    definimos helper para JSONBackend

--- staticconfiguration/json_backend/json_backend.py
+++ staticconfiguration/json_backend/json_backend.py
@@ -3,11 +3,14 @@ from pathlib import Path
 from staticconfiguration.entities import Data
 
 class JSONBackend:
-    def ensure_initialized(self, config_path: str, config_file: str, version: str, data_fields: list[Data]) -> None:
+    def ensure_initialized(self, config_path: str, version: str, data_fields: list[Data]) -> None:
         raise NotImplementedError("Subclasses must implement the get method.")
 
     def read_value(self, data: Data, config_path: Path):
         raise NotImplementedError("Subclasses must implement the get method.")
 
     def write_value(self, data: Data, new_value, config_path: Path):
+        raise NotImplementedError("Subclasses must implement the get method.")
+    
+    def _init_json_file(self, config_path: Path, version: str, data_fields: list[Data]) -> None:
         raise NotImplementedError("Subclasses must implement the get method.")
